7cb118ef46fc58ff9ed23df81a4cab1195bc70a3
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-05-30 17:32:58 -0700

    solved in py as well

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,19 @@
 #REMEMBER TO PSEUDOCODE
 def pad(array, min_size, value = None):
+
+    padCount = 0
+    answer = ""
+    fillLen = min_size - len(array)
+    if fillLen > 1:
+        while padCount < fillLen:
+            array.append(value)
+            padCount += 1
+    
+    answer = array
+    return answer
+
+print(pad([1,2,3],7,"p"))
+print(pad(["w","w","w"],10,"e"))
+print(pad(["fox","box"],2,"p"))
+print(pad(["fox", "box","fox", "box"],8,"sox!"))
+print(pad([1,"two",3,"four"],12,"number"))
